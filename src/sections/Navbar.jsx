d613831157d41f95000a167561ef90199e3a10ba
Rafif <merafif28@gmail.com>
2025-08-10 12:24:13 +0700

    feat : add navbar and initial project

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { motion } from "motion/react";
+
+function Navigation() {
+  return (
+    <ul className="nav-ul">
+      <li className="nav-li">
+        <a className="nav-link" href="#home">
+          Home
+        </a>
+      </li>
+      <li className="nav-li">
+        <a className="nav-link" href="#about">
+          About
+        </a>
+      </li>
+      <li className="nav-li">
+        <a className="nav-link" href="#work">
+          Work
+        </a>
+      </li>
+      <li className="nav-li">
+        <a className="nav-link" href="#contact">
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export const Navbar = () => {
+  const [isOpen, SetIsOpen] = useState(false);
+  return (
+    <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
+      <div className="mx-auto c-space max-w-7xl">
+        <div className="flex justify-between items-center py-2 my-2 sm:py-0">
+          <a
+            href="/"
+            className="text-2xl font-bold transition-colors hover:text-white text-neutral-400"
+          >
+            Yoapipp
+          </a>
+
+          <button
+            onClick={() => SetIsOpen(!isOpen)}
+            className="flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              className="w-7 h-7"
+              alt="toggle"
+            />
+          </button>
+          <nav className="hidden sm:flex sm:">
+            <Navigation />
+          </nav>
+        </div>
+      </div>
+      {isOpen && (
+        <motion.div
+          className="block overflow-hidden text-center sm:hidden"
+          initial={{ opacity: 0, x: -30 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{duration: 1}}
+          style={{maxHeight: "100vh"}}
+        >
+          <nav className="pb-5">
+            <Navigation />
+          </nav>
+        </motion.div>
+      )}
+    </div>
+  );
+};
